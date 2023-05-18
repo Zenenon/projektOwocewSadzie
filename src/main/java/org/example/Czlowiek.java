900823a177f7a01f09efc40750def04f1fa99e66
Zenenon <272938@student.pwr.edu.pl>
2023-05-18 09:42:52 +0200

    dodanie klasy drzewo i człowiek

--- src/main/java/org/example/Czlowiek.java
+++ src/main/java/org/example/Czlowiek.java
@@ -1,2 +1,28 @@
-package org.example;public class Czlowiek {
+package org.example;
+
+public class Czlowiek {
+    private String imie;
+    private double szybkosc;
+    private int max_pracy;
+    private int pozycja;
+
+    public Czlowiek(String imie, double szybkosc, int max_pracy, int pozycja) {
+        this.imie = imie;
+        this.szybkosc = szybkosc;
+        this.max_pracy = max_pracy;
+        this.pozycja = pozycja;
+    }
+    public String getImie(){
+        return this.imie;
+    }
+    public double getSzybkosc(){
+        return this.szybkosc;
+    }
+    public double getMax_pracy(){
+        return this.max_pracy;
+    }
+    public double getPozycja(){return this.pozycja;
+    }
 }
+
+
--- src/main/java/org/example/Drzewo.java
+++ src/main/java/org/example/Drzewo.java
@@ -1,2 +1,24 @@
-package org.example;public class Drzewo {
+package org.example;
+
+public class Drzewo {
+    private String nazwa;
+    private int ile_owocow;
+    private int rodzaj_owocow;
+    private boolean czy_wysokie;
+    public Drzewo(String nazwa, int ile_owocow, int rodzaj_owocow, boolean czy_wysokie) {
+        this.nazwa = nazwa;
+        this.ile_owocow = ile_owocow;
+        this.rodzaj_owocow = rodzaj_owocow;
+        this.czy_wysokie = czy_wysokie;
+    }
+    public String getNazwa(){
+        return this.nazwa;
+    }
+    public int getIle_owocow(){
+        return this.ile_owocow;
+    }
+    public int getRodzaj_owocow(){
+        return this.rodzaj_owocow;
+    }
+    public boolean getCzy_wysokie(){return this.czy_wysokie;}
 }

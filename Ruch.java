e2fba4f84b996c30f4ecb86a2cfd978582a0c382
Zenenon <133888202+Zenenon@users.noreply.github.com>
2023-05-23 12:12:25 +0200

    dodanie klasy krzew, zarys ruchu po planszy

---
+++ Ruch.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface Ruch {
+
+    // funkcja odpowiada za poruszanie sie obiektow typu czlowiek
+    int zbierz_owoc();
+    // funkcja ma dodac obiekcie czlowiek +1 jak zbierze wowc
+    int cofnij();
+    // funkcja ma cofnac obiekt w kolejnej rundzie do poprzedniej pozycji jesli minimum 2 obiekty typu czlowiek spotkaja sie na tej samej pozycji
+}

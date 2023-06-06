4eced29bb2f1a1498ea9f23ded4683d759cf41b7
weronikagut <134200118+weronikagut@users.noreply.github.com>
2023-06-06 10:57:53 +0200

    Update Dorosly.java

--- src/main/java/org/example/Dorosly.java
+++ src/main/java/org/example/Dorosly.java
@@ -6,6 +6,6 @@ public class Dorosly extends Czlowiek{
     public Dorosly(String imie, int max_pracy, int pozycja_x, int pozycja_y, int zebrane_owoce, int tura) {
         super(imie, max_pracy, pozycja_x, pozycja_y, zebrane_owoce, tura);
     }
-    ArrayList<Dorosly> dorosly = new ArrayList<>();
+    //ArrayList<Dorosly> dorosly = new ArrayList<>();
 
 }

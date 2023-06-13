ac90d2bc213f94c61928622d93e22811d75fa83d
weronikagut <134200118+weronikagut@users.noreply.github.com>
2023-06-13 16:44:42 +0200

    Update Dorosly.java
    dorosły ładnie

--- src/main/java/org/example/Dorosly.java
+++ src/main/java/org/example/Dorosly.java
@@ -1,11 +1,9 @@
 package org.example;
-import java.util.ArrayList;
-
 
 public class Dorosly extends Czlowiek{
-    public Dorosly(String imie, int max_pracy, int pozycja_x, int pozycja_y, int zebrane_owoce, int tura) {
-        super(imie, max_pracy, pozycja_x, pozycja_y, zebrane_owoce, tura);
+    public Dorosly(String imie, int zebrane_owoce, int max_pracy, int pozycja_x, int pozycja_y, int ile_przerw) {
+        super(imie, zebrane_owoce, max_pracy, pozycja_x, pozycja_y, ile_przerw);
     }
-    //ArrayList<Dorosly> dorosly = new ArrayList<>();
 
 }
+
